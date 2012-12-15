77f9687929c200b085c93b370e312211b2e43651
Matias <matiasdecarli@gmail.com>
2012-12-15 16:33:08 -0300

    new changes

--- getEvents.php
+++ getEvents.php
@@ -12,8 +12,34 @@
 	$sql = "SELECT * FROM event";
 	$res = mysql_query($sql);
 
-	while ($fila = mysql_fetch_array($res)){
-		$data[]= $fila['name'].';'.$fila['location'];
+	while ($fila = mysql_fetch_array($res)){			
+
+		$sql2= 'SELECT *
+				FROM hashtag as h 
+				     join event_hash as e_h on e_h.id_hashtag = h.id
+				     join event as e on e_h.id_event = e.id
+				WHERE 
+				     e.id = '.$fila['id'];
+		$res2 = mysql_query($sql2);
+
+		$hashtags='';
+		while ($fila2 = mysql_fetch_array($res2)){		
+
+			$hashtags.= $fila2['hashtag'].',';
+
+		}		     
+		$hashtags = substr($hashtags,0,strlen($hashtags)-1);
+
+		//var_dump($hashtags);
+
+
+		if($hashtags){
+			$data[]= $fila['name'].';'.$fila['location'].';'.$hashtags;
+		}
+		else{
+			$data[]= $fila['name'].';'.$fila['location'].';()';	
+		}
+
 	}	
 
 	echo json_encode($data);
